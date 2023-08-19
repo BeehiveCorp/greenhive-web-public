b8570f7bf8831d5ef8a16ded07454a2f0e3299c8
Almeida154 <davidalmeida154of@gmail.com>
2023-08-19 18:09:37 -0300

    add palette

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Navbar from '@/components/Navbar';
 import localFont from 'next/font/local';
 import { Montserrat } from 'next/font/google';
@@ -5,14 +7,18 @@ import { Montserrat } from 'next/font/google';
 const montserrat = Montserrat({ subsets: ['latin'] });
 const kyivTypeSans = localFont({ src: '../assets/fonts/KyivType-Sans-Medium-.otf' });
 
+import { Providers } from './providers';
+
 import '@/theme/global.scss';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={montserrat.className}>
         <Navbar />
-        {children}
+        <div className="theme--dark">
+          <Providers>{children}</Providers>
+        </div>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-import { Metadata } from "next";
+import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Greenhive - Home',
-}
+};
+
+import './styles.scss';
 
 export default function Home() {
-  return <h1>Landing Page</h1>;
+  return (
+    <div className="container">
+      <h1 className="title">Titulo</h1>
+      <h1 className="text">Parágrafo</h1>
+
+      <div className="detail"></div>
+    </div>
+  );
 }
